e895eea88d1b665fd4d9b7369e5fff5bbcab1c78
paulomaia4 <paulomaia4@hotmail.com>
2022-04-23 22:18:28 -0300

    endpoint Tecnico FindById criado

---
+++ src/main/java/com/paulomaia/helpdesk/services/TecnicoService.java
@@ -0,0 +1,24 @@
+package com.paulomaia.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulomaia.helpdesk.domain.Tecnico;
+import com.paulomaia.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+	
+
+}

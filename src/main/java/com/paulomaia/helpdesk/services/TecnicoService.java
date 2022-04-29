0b07895efdd664fe62406d3e89a8d9a68263bb3d
paulomaia4 <paulomaia4@hotmail.com>
2022-04-28 21:59:20 -0300

    exceção para objeto não localizado

--- src/main/java/com/paulomaia/helpdesk/services/TecnicoService.java
+++ src/main/java/com/paulomaia/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.paulomaia.helpdesk.domain.Tecnico;
 import com.paulomaia.helpdesk.repositories.TecnicoRepository;
+import com.paulomaia.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -17,7 +18,7 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " +id));
 	}
 	
 

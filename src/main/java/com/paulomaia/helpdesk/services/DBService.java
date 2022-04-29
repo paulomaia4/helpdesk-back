e57cb2c4d3b158338174ca0cf8cf436006319ea6
paulomaia4 <paulomaia4@hotmail.com>
2022-04-28 22:18:32 -0300

    endpoint findAll para tecnico

--- src/main/java/com/paulomaia/helpdesk/services/DBService.java
+++ src/main/java/com/paulomaia/helpdesk/services/DBService.java
@@ -21,7 +21,7 @@ public class DBService {
 	
 
 	@Autowired
-	private TecnicoRepository tecniRepository;
+	private TecnicoRepository tecnicoRepository;
 	@Autowired
 	private ClienteRepository clienteRepository;
 	@Autowired
@@ -31,13 +31,15 @@ public class DBService {
 	public void instaciaDB() {
 	
 	Tecnico tec1 = new Tecnico(null, "Paulo Maia", "12345678910", "paulo@mail.com", "123");
+	Tecnico tec2 = new Tecnico(null, "Pedro Maia", "68564879753", "pepeo@mail.com", "123");
+	Tecnico tec3 = new Tecnico(null, "Steve Jobs", "91078945635", "steveo@mail.com", "123");
 	tec1.addPerfil(Perfil.ADMIN);
 	
 	Cliente cli1 = new Cliente(null, "Pedro Maia", "10987654321", "pedro@mail.com", "123");
 	
 	Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
 	
-	tecniRepository.saveAll(Arrays.asList(tec1));
+	tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3));
 	clienteRepository.saveAll(Arrays.asList(cli1));
 	chamadoRepository.saveAll(Arrays.asList(c1));
 }

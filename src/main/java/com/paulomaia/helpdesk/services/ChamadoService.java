ab27619c48c260354906bf7fd62be0178497bc69
paulomaia4 <paulomaia4@hotmail.com>
2022-05-03 22:25:59 -0300

    findall para chamado

--- src/main/java/com/paulomaia/helpdesk/services/ChamadoService.java
+++ src/main/java/com/paulomaia/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.paulomaia.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,11 @@ public class ChamadoService {
 		
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		
+		return repository.findAll();
 	}
 	
 }

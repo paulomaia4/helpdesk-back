a3f68c11af8a637867a1eb121cc4682156a02d58
paulomaia4 <paulomaia4@hotmail.com>
2022-05-06 21:57:49 -0300

    autenticacao jwt

--- src/main/java/com/paulomaia/helpdesk/services/DBService.java
+++ src/main/java/com/paulomaia/helpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.paulomaia.helpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.paulomaia.helpdesk.domain.Chamado;
@@ -12,8 +13,7 @@ import com.paulomaia.helpdesk.domain.enums.Perfil;
 import com.paulomaia.helpdesk.domain.enums.Prioridade;
 import com.paulomaia.helpdesk.domain.enums.Status;
 import com.paulomaia.helpdesk.repositories.ChamadoRepository;
-import com.paulomaia.helpdesk.repositories.ClienteRepository;
-import com.paulomaia.helpdesk.repositories.TecnicoRepository;
+import com.paulomaia.helpdesk.repositories.PessoaRepository;
 
 @Service
 public class DBService {
@@ -21,26 +21,36 @@ public class DBService {
 	
 
 	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	
 	public void instaciaDB() {
 	
-	Tecnico tec1 = new Tecnico(null, "Paulo Maia", "123.456.789-10", "paulo@mail.com", "123");
-	Tecnico tec2 = new Tecnico(null, "Pedro Maia", "685.648.797-53", "pepeo@mail.com", "123");
-	Tecnico tec3 = new Tecnico(null, "Steve Jobs", "910.789.456-35", "steveo@mail.com", "123");
-	tec1.addPerfil(Perfil.ADMIN);
-	
-	Cliente cli1 = new Cliente(null, "Pedro Maia", "10987654321", "pedro@mail.com", "123");
-	
-	Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
-	
-	tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3));
-	clienteRepository.saveAll(Arrays.asList(cli1));
-	chamadoRepository.saveAll(Arrays.asList(c1));
+		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "550.482.150-95", "valdir@mail.com", encoder.encode("123"));
+		tec1.addPerfil(Perfil.ADMIN);
+		Tecnico tec2 = new Tecnico(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", encoder.encode("123"));
+		Tecnico tec3 = new Tecnico(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", encoder.encode("123"));
+		Tecnico tec4 = new Tecnico(null, "Tim Berners-Lee", "162.720.120-39", "lee@mail.com", encoder.encode("123"));
+		Tecnico tec5 = new Tecnico(null, "Linus Torvalds", "778.556.170-27", "linus@mail.com", encoder.encode("123"));
+
+		Cliente cli1 = new Cliente(null, "Albert Einstein", "111.661.890-74", "einstein@mail.com", encoder.encode("123"));
+		Cliente cli2 = new Cliente(null, "Marie Curie", "322.429.140-06", "curie@mail.com", encoder.encode("123"));
+		Cliente cli3 = new Cliente(null, "Charles Darwin", "792.043.830-62", "darwin@mail.com", encoder.encode("123"));
+		Cliente cli4 = new Cliente(null, "Stephen Hawking", "177.409.680-30", "hawking@mail.com", encoder.encode("123"));
+		Cliente cli5 = new Cliente(null, "Max Planck", "081.399.300-83", "planck@mail.com", encoder.encode("123"));
+ 
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Teste chamado 1", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 2", "Teste chamado 2", tec1, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 3", "Teste chamado 3", tec2, cli3);
+		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 4", "Teste chamado 4", tec3, cli3);
+		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 5", "Teste chamado 5", tec2, cli1);
+		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 7", "Teste chamado 6", tec1, cli5);
+
+		pessoaRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5, cli1, cli2, cli3, cli4, cli5));
+		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6));
 }
 }
